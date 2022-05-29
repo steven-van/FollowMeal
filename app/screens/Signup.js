cd2023a4d42f0e0b1fee9a19ad810e80c768fc68
steven-van <64859587+steven-van@users.noreply.github.com>
2022-05-29 14:27:05 +0200

    feat: signup

--- app/screens/Signup.js
+++ app/screens/Signup.js
@@ -11,18 +11,45 @@ import { StyleSheet, Image } from "react-native";
 import { RED } from "../components/config.js";
 
 const SignupContainer = styled.View`
-  width: 60%;
-  margin-bottom: 30px;
+  margin-bottom: 15px;
 `;
 
 const InputContainer = styled.View`
-  margin-top: 20px;
+  margin-top: 15px;
+`;
+
+const FormContainer = styled.View`
+  margin-bottom: 20px;
+`;
+
+const FormInputContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  margin-top: 15px;
+`;
+
+const ScrollViewContainer = styled.ScrollView`
+  width: 80%;
+  margin: 10px 0;
+`;
+
+const Unit = styled.Text`
+  font-size: 12px;
+  font-family: "FredokaOne";
+  width: 60px;
+  text-align: center;
 `;
 
 const Signup = ({ navigation }) => {
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
   const [confirm, setConfirm] = useState("");
+  const [age, setAge] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [sportPerWeek, setSportPerWeek] = useState(0);
+  const [pricePerMeal, setPricePerMeal] = useState(0);
 
   const handleSignup = async (credentials) => {
     return fetch(`http://${host}:3000/auth/signup`, {
@@ -40,62 +67,134 @@ const Signup = ({ navigation }) => {
   return (
     <SafeContainer>
       <Image style={styles.mediumIcon} source={ICON} />
-
       <Title fontSize={"25px"} additionnalStyle={{ marginTop: 30 }}>
         {"Inscription"}
       </Title>
-      <SignupContainer>
-        <InputContainer>
-          <InputLabel additionnalStyle={{ marginBottom: 9 }}>
-            {"Identifiant"}
-          </InputLabel>
-          <Input placeholder={"Email"} onChangeText={(text) => setUser(text)} />
-        </InputContainer>
+      <ScrollViewContainer>
+        <SignupContainer>
+          <InputContainer>
+            <InputLabel additionnalStyle={{ marginBottom: 5 }}>
+              {"Identifiant"}
+            </InputLabel>
+            <Input placeholder={"Email"} onChangeText={(e) => setUser(e)} />
+          </InputContainer>
 
-        <InputContainer>
-          <InputLabel additionnalStyle={{ marginBottom: 9 }}>
-            {"Mot de passe"}
-          </InputLabel>
-          <Input
-            placeholder={"Mot de passe"}
-            onChangeText={(text) => setPassword(text)}
-            secureTextEntry={true}
-          ></Input>
-        </InputContainer>
+          <InputContainer>
+            <InputLabel additionnalStyle={{ marginBottom: 5 }}>
+              {"Mot de passe"}
+            </InputLabel>
+            <Input
+              placeholder={"Mot de passe"}
+              onChangeText={(e) => setPassword(e)}
+              secureTextEntry={true}
+            ></Input>
+          </InputContainer>
 
-        <InputContainer>
-          <InputLabel additionnalStyle={{ marginBottom: 9 }}>
-            {"Confirmation"}
-          </InputLabel>
-          <Input
-            placeholder={"Confirmer le mot de passe"}
-            onChangeText={(text) => setConfirm(text)}
-            secureTextEntry={true}
-          ></Input>
-        </InputContainer>
-      </SignupContainer>
+          <InputContainer>
+            <InputLabel additionnalStyle={{ marginBottom: 5 }}>
+              {"Confirmation"}
+            </InputLabel>
+            <Input
+              placeholder={"Confirmer le mot de passe"}
+              onChangeText={(e) => setConfirm(e)}
+              secureTextEntry={true}
+            ></Input>
+          </InputContainer>
+          {password.length > 0 && confirm.length > 0 && confirm != password && (
+            <Title
+              fontSize={"12px"}
+              additionnalStyle={{
+                textAlign: "center",
+                marginTop: 5,
+                color: RED,
+              }}
+            >
+              {"Les mots de passe ne correspondent pas"}
+            </Title>
+          )}
+        </SignupContainer>
 
-      {password.length > 0 && confirm.length > 0 && confirm != password && (
-        <Title
-          fontSize={"12px"}
-          additionnalStyle={{
-            width: "70%",
-            textAlign: "center",
-            marginBottom: 20,
-            color: RED,
-          }}
+        <FormContainer>
+          <FormInputContainer>
+            <InputLabel additionnalStyle={styles.inputTag}>{"Age"}</InputLabel>
+            <Input
+              type={"numeric"}
+              onChangeText={(e) => setAge(e)}
+              maxLength={2}
+              placeholder={"Ex : 18"}
+            />
+            <Unit>{""}</Unit>
+          </FormInputContainer>
+
+          <FormInputContainer>
+            <InputLabel additionnalStyle={styles.inputTag}>
+              {"Taille"}
+            </InputLabel>
+            <Input
+              type={"numeric"}
+              maxLength={3}
+              onChangeText={(e) => setHeight(e)}
+              placeholder={"Ex : 170"}
+            />
+            <Unit>{"cm"}</Unit>
+          </FormInputContainer>
+
+          <FormInputContainer>
+            <InputLabel additionnalStyle={styles.inputTag}>
+              {"Poids"}
+            </InputLabel>
+            <Input
+              width={"200px"}
+              type={"numeric"}
+              maxLength={3}
+              onChangeText={(e) => setWeight(e)}
+              placeholder={"Ex : 60"}
+            />
+            <Unit>{"kg"}</Unit>
+          </FormInputContainer>
+
+          <FormInputContainer>
+            <InputLabel additionnalStyle={styles.inputTag}>
+              {"Pratique sportive"}
+            </InputLabel>
+            <Input
+              type={"numeric"}
+              maxLength={1}
+              onChangeText={(e) => setSportPerWeek(e)}
+              placeholder={"Ex : 3"}
+            />
+            <Unit>{"jrs / sem"}</Unit>
+          </FormInputContainer>
+
+          <FormInputContainer>
+            <InputLabel additionnalStyle={styles.inputTag}>
+              {"Objectif"}
+            </InputLabel>
+            <Input
+              type={"numeric"}
+              maxLength={3}
+              onChangeText={(e) => setPricePerMeal(e)}
+              placeholder={"Ex : 18"}
+            />
+            <Unit>{"€ / repas"}</Unit>
+          </FormInputContainer>
+        </FormContainer>
+        <Button
+          handlePress={() =>
+            console.log(
+              user,
+              password,
+              age,
+              height,
+              weight,
+              sportPerWeek,
+              pricePerMeal
+            )
+          }
         >
-          {"Les mots de passe ne correspondent pas"}
-        </Title>
-      )}
-
-      <Button
-        handlePress={() =>
-          /*handleSignup({user: user, password: password})*/ ""
-        }
-      >
-        {"S'inscrire"}
-      </Button>
+          {"S'inscrire"}
+        </Button>
+      </ScrollViewContainer>
 
       <StatusBar style="auto" />
     </SafeContainer>
@@ -106,4 +205,7 @@ export default Signup;
 
 const styles = StyleSheet.create({
   ...STYLE,
+  inputTag: {
+    width: 85,
+  },
 });
