a18a4b3c171d707af6175eb8e7c4e964f4247c94
tomx93 <xie.tom7@gmail.com>
2022-05-30 16:12:41 +0200

    add progress line

--- app/screens/User.js
+++ app/screens/User.js
@@ -1,19 +1,24 @@
 import { useEffect, useState, useRef } from 'react'
 import styled from "styled-components/native";
 import SafeContainer from "../components/SafeContainer";
+import Title from "../components/Title";
 import { Text, View, Dimensions } from "react-native";
 import Svg from "react-native-svg";
-import { LineChart } from "react-native-chart-kit";
+import { LineChart, ProgressChart } from "react-native-chart-kit";
 import { host } from "../config/host";
 import { FredokaOne_400Regular } from "@expo-google-fonts/fredoka-one";
+
+const screenWidth = Dimensions.get("window").width - 30;
 
 const User = ({route, navigation}) => {
 
     const userdata = route.params;
-    console.log(userdata.id);
     const [meals,setMeals] = useState(null);
     const [dates,setDates] = useState(null);
     const [scores,setScores] = useState(null);
+
+    const [progressData, setProgressData] = useState(null);
+
     useEffect(() => {
         async function prepare() {
             try{
@@ -26,15 +31,31 @@ const User = ({route, navigation}) => {
                 .then((response) => response.json())
                 .then(json =>  {
                     setMeals(json)
+                    
+                    var recentScores = json[json.length - 1];
+                    const progress = {
+                        labels:["Score nutritif", "Score budgetaire", "Score total"],
+                        data:[recentScores.nutr_score/100, recentScores.budg_score/100, recentScores.score/100],
+                        colors: [
+                            "rgba(255, 0, 0,0.5)",
+                            "rgba(238, 130, 238,0.6)",
+                            "rgba(106, 90, 205,0.5)",
+                          ],
+                    };
+
                     var objd = [];
-                    var objs = []
+                    var objs = [];
                     const item = json.map(e =>{
                         objd.push(e.date);
                         objs.push(e.score);
                     })
+
+                    setProgressData(progress);
                     setDates(objd);
                     setScores(objs);
                 });
+
+
             } catch (e) {
                 console.warn(e);
             }
@@ -43,57 +64,66 @@ const User = ({route, navigation}) => {
         if(meals!=null){
             CreateLineChart(dates,scores)
         }
+
+        return () => {
+
+        }
     },[]);
 
     const CreateLineChart = (props) => {
-        return(
+        return (
             <LineChart
-    data={{
-      labels:props.dates,
-      datasets: [
-        {
-          data: props.scores
-        }
-      ]
-    }}
-    width={Dimensions.get("window").width - 30} // from react-native
-    height={220}
-    fromZero = {true}
-    yAxisLabel="Score "
-    yAxisInterval={1} // optional, defaults to 1
-    chartConfig={{
-      backgroundColor: "#73D28f",
-      backgroundGradientFrom: "#73D28f",
-      backgroundGradientTo: "#73D28f",
-      decimalPlaces: 2, // optional, defaults to 2dp
-      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      style: {
-        borderRadius: 16
-      },
-      propsForDots: {
-        r: "6",
-        strokeWidth: "2",
-        stroke: "#DE4E4E"
-      },
-      propsForLabels:{
-        fontSize : "10",
-    }
-    }}
+            data={{
+                labels:props.dates,
+                datasets: [
+                    {
+                        data: props.scores
+                    },
+                    {
+                        data: [0], // min
+                        withDots: false, //a flage to make it hidden
+                    },
+                    {
+                        data: [100], // max
+                        withDots: false, //a flage to make it hidden
+                    },
+                ]
+            }}
+            width={screenWidth} // from react-native
+            height={220}
+            fromZero = {true}
+            // yAxisLabel="Score "
+            yAxisInterval={100} // optional, defaults to 1
+            onDataPointClick={() => {}}
+            chartConfig={{
+                backgroundColor: "#73D28f",
+                backgroundGradientFrom: "#73D28f",
+                backgroundGradientTo: "#73D28f",
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style: {
+                    borderRadius: 16
+                },
+                propsForDots: {
+                    r: "4",
+                    strokeWidth: "2",
+                    stroke: "#DE4E4E"
+                },
+                propsForLabels:{
+                    fontSize : "10",
+                }
+            }}
 
-    bezier
-    style={{
-      marginVertical: 8,
-      borderRadius: 16
-    }}
-  />
-        )
-    }
+            bezier
+            style={{
+            marginVertical: 8,
+            borderRadius: 16
+            }}
+            />
+    )}
 
-    console.log(meals);
-    // console.log(meals[0].date);
-    console.log(dates)
-    if(meals ==null || dates == null || scores == null){
+    if(meals ==null || dates == null || scores == null || progressData == null){
         return(
         <SafeContainer>
 
@@ -103,10 +133,30 @@ const User = ({route, navigation}) => {
 
     return(
         <SafeContainer>
-        <Text>
-            {'bonjour ' + userdata.username}
-            {"\n"}
-        </Text>
+        <Title fontSize={"26px"} additionnalStyle={{marginBottom:10}}>
+            {'Bonjour ' + userdata.username}
+        </Title>
+        <View>
+            <ProgressChart 
+            data={progressData}
+            width={screenWidth}
+            height={220}
+            strokeWidth={16}
+            radius={32}
+            chartConfig={{
+                backgroundColor: "#73D28f",
+                backgroundGradientFrom: "#73D28f",
+                backgroundGradientTo: "#73D28f",
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                propsForLabels:{
+                    fontSize : "10",
+                }
+            }}
+            hideLegend={false}
+            />
+        </View>
         <View>
             <CreateLineChart dates = {dates} scores = {scores}/>
         </View>
